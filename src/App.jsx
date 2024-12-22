444604d052e0517bfe78cae813693a57ffa3819b
Casey-Page <caseypage@mail.com>
2024-12-22 23:39:43 +0000

    Show no project screen when no projects selected, show new project otherwise

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import Sidebar from './components/Sidebar';
 import NewProject from "./components/NewProject.jsx";
 import NoProject from "./components/NoProject.jsx";
 
 function App() {
-  return (
 
+    const [projectState, setProjectState] = useState({
+        selectedProjectID: undefined,  // Store selected projects ID
+        projects: []  // Array of the created projects
+    });
 
+    function handleStartAddProject (){
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectID: null,
+            };
+        });
+    }
 
+    let content;
+
+    if (projectState.selectedProjectID === null) {
+        content = <NewProject />
+    } else if (projectState.selectedProjectID === undefined) {
+        content = <NoProject onStartAddProject={handleStartAddProject}/>
+    }
+
+    return (
     <>
         <main className="h-screen my-8 flex gap-8">
             <Sidebar />
-            <NoProject />
+            {content}
         </main>
     </>
   );
--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -2,7 +2,7 @@ import Button from "./Button.jsx";
 
 const noProjectImg = './src/assets/no-projects.png'
 
-export default function NoProject () {
+export default function NoProject ({ onStartAddProject }) {
     return (
         <div className="text-center mt-24 w-2/3">
             <img
@@ -13,7 +13,7 @@ export default function NoProject () {
             <h2 className="text-xl font-bold text-stone-500 my-4">No project selected</h2>
             <p className="text-stone-400">Select a project or get started with a new one.</p>
             <p className="mt-8">
-                <Button>Create a new project</Button>
+                <Button onClick={onStartAddProject}>Create a new project</Button>
             </p>
 
         </div>
