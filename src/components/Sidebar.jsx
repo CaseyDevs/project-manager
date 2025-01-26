00f9b1a5da83e0d26c907616b590ac6798bd2cce
Casey-Page <caseypage@mail.com>
2025-01-26 20:13:05 +0000

    Minor styling fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button.jsx";
 import { useState } from "react"
 
-export default function Sidebar({ projectList, onHandleStartAddProject, onSelectedProject, selectedProjectId }){
+export default function Sidebar({ projectList, onHandleStartAddProject, onSelectedProject, selectedProjectId }) {
     return (
         <aside className="sidebar px-10 py-20 w-1/4 flex-initial flex-col rounded-r-2xl bg-black text-white">
-            <h2 className="font-medium text-2xl">YOUR PROJECTS</h2>
+            <h2 className="font-medium lg:text-2xl sm:text-xl xs:text-s">YOUR PROJECTS</h2>
             <p>
                 <Button onClick={onHandleStartAddProject}>+ Add Project</Button>
             </p>
@@ -12,27 +12,27 @@ export default function Sidebar({ projectList, onHandleStartAddProject, onSelect
                 {/* Map each existing project into list items */}
                 {projectList.map((project) => {
 
-                    let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800"
-                    
+                    let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800 overflow-hidden"
+
                     // Set active class on the sele
                     if (project.id === selectedProjectId) {
                         cssClasses += " bg-stone-800 text-stone-200"
                     } else {
                         cssClasses += " text-stone-400"
                     }
-                    
+
 
                     // Return a list of created projects in the sidebar
                     return (
-                    <li key={project.id}>
-                        <button 
-                            className={cssClasses}
-                            onClick={() => onSelectedProject(project.id)}
-                        >
-                            {project.title}
-                            
-                        </button>
-                    </li>
+                        <li key={project.id}>
+                            <button
+                                className={cssClasses}
+                                onClick={() => onSelectedProject(project.id)}
+                            >
+                                {project.title}
+
+                            </button>
+                        </li>
                     );
                 })}
             </ul>
